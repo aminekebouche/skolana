9826cb6e2b9e32a3e38476aeef60bc722a2be903
Boukhalfa HAMITOUCHE <66253064+Boukhalfa11@users.noreply.github.com>
2024-04-07 14:24:55 +0200

    Delete front-end-nextjs/src/library/tezos directory

--- front-end-nextjs/src/library/tezos/tezos.js
+++
@@ -1,6 +0,0 @@
-import { TezosToolkit } from "@taquito/taquito";
-import { wallet } from "./wallet";
-export const tezos = new TezosToolkit("https://ghostnet.ecadinfra.com");
-
-
-tezos.setWalletProvider(wallet)
